dfef64866f97689f61f707702cfc3ede6536d824
Lalit Punjabi <lalitpunjabi5@gmail.com>
2026-05-16 23:01:40 +0530

    Resume updated 2026

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -52,7 +52,7 @@ export default function HeroSection() {
             </a>
 
             <a
-              href="https://drive.google.com/file/d/1ynMOZFQxID_vvJbIkftTcVKIsHLP3_lk/view?usp=sharing"
+              href="https://drive.google.com/file/d/158xSoukZJyVPqgBgqDw8TeZqM46yKI3c/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
               className="btn btn-outline"
